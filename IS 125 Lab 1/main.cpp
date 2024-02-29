7fd8eef93492b5dbe1fa6391c811b5a7f6558523
Josue Mora <josue@gocodistry.com>
2024-02-28 20:21:31 -0800

    Initial Commit

---
+++ IS 125 Lab 1/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  IS 125 Lab 1
+//
+//  Created by Josue on 2/28/24.
+//
+
+#include <iostream>
+#include <string>
+
+class Sets {
+private:
+  // tbd
+
+public:
+  // constructor
+  Sets() = default;
+
+};
+
+int main(int argc, const char * argv[]) {
+    // create an instance of a set
+    Sets set1;
+
+    return 0;
+}
